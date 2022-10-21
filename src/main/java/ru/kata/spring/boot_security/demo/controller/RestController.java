20995328ae9df113e6ac039a9f2e9f0948a739cc
re1ard <mr.little.cat.ru@gmail.com>
2022-10-21 09:44:37 +0300

    rest moment

---
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/RestController.java
@@ -0,0 +1,61 @@
+package ru.kata.spring.boot_security.demo.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.kata.spring.boot_security.demo.model.Role;
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.service.UserServiceImpl;
+
+import java.security.Principal;
+import java.util.Collection;
+import java.util.List;
+
+@org.springframework.web.bind.annotation.RestController
+@RequestMapping("/api")
+public class RestController {
+    private final UserServiceImpl userService;
+
+    public RestController(UserServiceImpl userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> GetUsers(){
+        return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
+    }
+
+    @GetMapping("/users/{id}")
+    public ResponseEntity<User> GetUser(@PathVariable long id){
+        User user = userService.getUser(id);
+        return (user != null) ? new ResponseEntity<>(user, HttpStatus.OK) : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<User> AddUser(@RequestBody User user){
+        userService.save(user);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    @PatchMapping("/users/{id}")
+    public ResponseEntity EditUser(@PathVariable long id, @RequestBody User user){
+        userService.update(user, id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/users/{id}")
+    public ResponseEntity DeleteUser(@PathVariable long id){
+        userService.deleteUser(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/user")
+    public ResponseEntity<User> CurrentUser(Principal user) {
+        return new ResponseEntity<>(userService.findByUsername(user.getName()), HttpStatus.OK);
+    }
+
+    @GetMapping("/roles")
+    public ResponseEntity<Collection<Role>> AllRoles() {
+        return new ResponseEntity<>(userService.GetRoles(), HttpStatus.OK);
+    }
+}
